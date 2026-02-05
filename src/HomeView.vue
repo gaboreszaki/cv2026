d91a021be313686dc6f57c30c19bdf4b94b96713
Gabor Eszaki <24547662+gaboreszaki@users.noreply.github.com>
2026-02-05 14:01:40 +0100

    v0.3

--- src/HomeView.vue
+++ src/HomeView.vue
@@ -3,26 +3,20 @@ import StatCard from "@/components/StatCard.vue";
 </script>
 
 <template>
+  <div class="pop-in">
 
-  <div class="hero text-capitalize">
+
+  <div class="hero text-capitalize ">
     <h1>A reliable software engineer with 25+ years of experience</h1>
-
     <ul class="pulse">
       <li class="tag ">Chief Technology Officer</li>
-
       <li class="tag">Lead & Senior Software Engineer</li>
-
       <li class="tag">DevOp and Cloud Engineer</li>
-
       <li class="tag">Infrastructure Architect</li>
-
       <li class="tag">Web & WebApp developer</li>
-
       <li class="tag">Scrum Master</li>
     </ul>
   </div>
-
-
 
   <div class="stats holder text-xxl">
     <i class="fa-brands fa-laravel"></i>
@@ -30,52 +24,65 @@ import StatCard from "@/components/StatCard.vue";
     <i class="fa-brands fa-vuejs"></i>
     <i class="fa-brands fa-square-js"></i>
     <i class="fa-brands fa-python"></i>
+
     <i class="fa-brands fa-docker"></i>
     <i class="fa-brands fa-git-alt"></i>
     <i class="fa-brands fa-github"></i>
     <i class="fa-brands fa-digital-ocean"></i>
     <i class="fa-brands fa-aws"></i>
     <i class="fa-brands fa-jira"></i>
-
   </div>
 
-
-  <div>
-
-
-  </div>
-
-
-  <div class="text-center mb-10 text-lg">
+  <div class="text-center my-10">
     <h3>I specialize in building High-Impact and Enterprise-Grade Web Apps.</h3>
 
     <p>My career is defined by a transition from deep technical craftsmanship to strategic leadership,<br>
       where I have successfully mentored engineering teams and driven technological innovation across international borders.</p>
 
     <p>
-      I am currently seeking new challenges in <strong>Ireland</strong> or the <strong>Netherlands</strong>,
-      bringing a proven track record of delivering stable, high-quality software solutions. My approach
-      is backed by a quarter-century of technical evolution—from selling my first Flash-based projects
-      in 2001 to architecting modern, enterprise-grade microservice ecosystems today.
-    </p>
-  </div>
-  <div class="stats text-capitalize">
+      I am currently seeking new challenges in <strong class="text-info">Ireland</strong> or the <strong class="text-info">Netherlands</strong>,<br>
+      bringing a proven track record of delivering stable,
+      high-quality software solutions. </p>
 
-    <stat-card title="BackEnd" :progress="10"/>
-    <stat-card title="FrontEnd" :progress="6"/>
-    <stat-card title="DevOps" :progress="8"/>
-    <stat-card title="Architect" :progress="7"/>
-    <stat-card title="management" :progress="8"/>
+    <div class="hero ">
+      <ul>
+        <li class="tag ">Available to Start</li>
+        <li class="tag ">Ready to Relocate</li>
+        <li class="tag">Citizen of EU</li>
+      </ul>
+    </div>
+
+  </div>
+  </div>
+  <div class="card pop-in">
+    <div class="card-body text-center">
+
+
+      <p class="mb-5">
+          <span class="text-lg">
+              My approach to software development
+           is backed by a quarter-century of technical evolution </span><br>
+        from selling my first Flash-based projects in 2001 to architecting modern, enterprise-grade microservice ecosystems today. </p>
+
+      <div class="stats text-capitalize">
+        <stat-card title="BackEnd" :progress="10"/>
+        <stat-card title="FrontEnd" :progress="6"/>
+        <stat-card title="DevOps" :progress="8"/>
+        <stat-card title="Architect" :progress="7"/>
+        <stat-card title="management" :progress="8"/>
+      </div>
+
+    </div>
   </div>
 
-  <div class="card mb-10">
+
+  <div class="card pop-in">
     <div class="card-body">
       <h3>Technical integrity over shortcuts.</h3>
       <p>
         In an industry increasingly saturated with automated, low-quality output,
         I provide expert-level engineering rooted in rigorous standards and logical precision.
-        My approach to software development is grounded in several core pillars:
-      </p>
+        My approach to software development is grounded in several core pillars: </p>
       <ul>
         <li><strong>Reliability:</strong> A commitment to <strong>Test-Driven Development (TDD)</strong> and <strong>PHPUnit</strong> to deliver stable, production-ready code.</li>
 
@@ -90,15 +97,14 @@ import StatCard from "@/components/StatCard.vue";
     </div>
   </div>
 
-  <div class="card mb-10">
+  <div class="card mb-10 pop-in">
     <div class="card-body">
       <h3>Beyond the codebase...</h3>
       <h5>My objective is to build the foundations that allow enterprises to thrive!</h5>
 
       <p>
         I bridge the gap between application logic and customer requirements,
-        ensuring that every line of code serves a clear strategic purpose and every system is built to endure.
-      </p>
+        ensuring that every line of code serves a clear strategic purpose and every system is built to endure. </p>
 
 
     </div>
